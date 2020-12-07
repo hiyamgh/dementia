81b9af33ef75a5b4a49fdcfd3acef36f4df9fbf2
Hiyam G <hkg02@mail.aub.edu>
2020-12-07 20:10:08 +0200

    added experiments 1 and 3 for fake news

--- Advanced-ML-Eval/run_fake_news_exp1.py
+++ Advanced-ML-Eval/run_fake_news_exp1.py
@@ -14,56 +14,56 @@ if __name__ == '__main__':
     models = ['ada_boost', 'decision_tree', 'extra_trees',
               'logistic_regression', 'random_forest']
 
-    trained_models_dir = 'input/fake_news_trained_models/Experiment1/'
-    model_objs = []
-    for model in models:
-        path_to_file = os.path.join(trained_models_dir, '{}.sav'.format(model))
-        if os.path.exists(path_to_file):
-            file_name = path_to_file
-        else:
-            path_to_file = os.path.join(trained_models_dir, '{}.pickle'.format(model))
+    experiments = ['Experiment1']
+    for exp in experiments:
+        trained_models_dir = 'input/fake_news_trained_models/{}/'.format(exp)
+        model_objs = []
+        for model in models:
+            path_to_file = os.path.join(trained_models_dir, '{}.sav'.format(model))
             if os.path.exists(path_to_file):
                 file_name = path_to_file
             else:
-                file_name = os.path.join(trained_models_dir, '{}.p'.format(model))
-        trained_model = pickle.load(open(file_name, 'rb'))
-        model_objs.append(trained_model)
+                path_to_file = os.path.join(trained_models_dir, '{}.pickle'.format(model))
+                if os.path.exists(path_to_file):
+                    file_name = path_to_file
+                else:
+                    file_name = os.path.join(trained_models_dir, '{}.p'.format(model))
+            trained_model = pickle.load(open(file_name, 'rb'))
+            model_objs.append(trained_model)
 
-    models_dict = dict(zip(models, model_objs))
+        models_dict = dict(zip(models, model_objs))
 
-    sm = ShallowModel(df=df, df_train=train_df, df_test=test_df,
-                      target_variable='label',
-                      plots_output_folder='plots/fake_news/',
-                      trained_models_dir='input/fake_news_trained_models/Experiment1/',
-                      models_dict=models_dict,
-                      scaling='z-score',
-                      cols_drop=cols_drop)
+        sm = ShallowModel(df=df, df_train=train_df, df_test=test_df,
+                          target_variable='label',
+                          plots_output_folder='plots/fake_news/{}/'.format(exp),
+                          trained_models_dir='input/fake_news_trained_models/{}/'.format(exp),
+                          models_dict=models_dict,
+                          scaling='z-score',
+                          cols_drop=cols_drop)
 
-    # identify frequent patterns in data
-    sm.identify_frequent_patterns()
+        # identify frequent patterns in data
+        sm.identify_frequent_patterns()
 
-    # advanced ML classification
-    for model in models:
-        print('\n~~~~~~~~~~~~~~~~~~~~~~~~ Model: {} ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'.format(model))
-        sm.classify(trained_model=models_dict[model], trained_model_name=model)
+        # advanced ML classification
+        for model in models:
+            print('\n~~~~~~~~~~~~~~~~~~~~~~~~ Model: {} ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'.format(model))
+            sm.classify(trained_model=models_dict[model], trained_model_name=model)
 
-    models_dict = dict(zip(models, model_objs))
+        # add mistake
+        sm.add_mistake()
 
-    # add mistake
-    sm.add_mistake()
+        # generate probabilities of mistakes per model per frequent pattern
+        probabilities_per_fp = sm.pattern_probability_of_mistake()
 
-    # generate probabilities of mistakes per model per frequent pattern
-    probabilities_per_fp = sm.pattern_probability_of_mistake()
+        # produce roc curves when applying classification using all models
+        sm.produce_roc_curves()
 
-    # produce roc curves when applying classification using all models
-    sm.produce_roc_curves()
+        # produce mean empirical risk curves
+        sm.produce_empirical_risk_curves()
 
-    # produce mean empirical risk curves
-    sm.produce_empirical_risk_curves()
+        # produce precision/recall at topK curves
+        sm.produce_curves_topK(topKs=[60, 50, 40, 30, 20, 10], metric='precision')
+        sm.produce_curves_topK(topKs=[60, 50, 40, 30, 20, 10], metric='recall')
 
-    # produce precision/recall at topK curves
-    sm.produce_curves_topK(topKs=[60, 50, 40, 30, 20, 10], metric='precision')
-    sm.produce_curves_topK(topKs=[60, 50, 40, 30, 20, 10], metric='recall')
-
-    # produce jaccard similarity at topK
-    sm.compute_jaccard_similarity(topKs=list(range(20, 200, 20)))
+        # produce jaccard similarity at topK
+        sm.compute_jaccard_similarity(topKs=list(range(20, 200, 20)))
