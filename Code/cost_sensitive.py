967fd52b343ce197085841cd7dc4b15a8fd64851
Roaa Al Feel <roaa.alfeel@gmail.com>
2020-12-28 18:03:32 +0200

    logistic cost sensitive

--- Code/cost_sensitive.py
+++ Code/cost_sensitive.py
@@ -1,23 +1,42 @@
+# weighted logistic regression model on an imbalanced classification dataset
+from collections import Counter
 from numpy import mean
+import numpy as np
+import pandas as pd
 from sklearn.datasets import make_classification
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 
-def logistic_regression(X,y):
-    model = LogisticRegression(solver='lbfgs')
-    # define evaluation procedure
-    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
-    # define grid search
-    grid = GridSearchCV(estimator=model, n_jobs=-1, cv=cv, scoring='roc_auc')
-    # execute the grid search
-    grid_result = grid.fit(X, y)
-    # report the best configuration
-    print('Best: %f using %s' % (grid_result.best_score_, grid_result.best_params_))
-    # report all configurations
-    means = grid_result.cv_results_['mean_test_score']
-    stds = grid_result.cv_results_['std_test_score']
-    params = grid_result.cv_results_['params']
-    print(mean, stdev, param)
+def baseline_logistic(X,y):
+    model = LogisticRegression()
+    scores = cross_val_score(model, X, y, scoring='roc_auc')
+    print('Baseline ROC AUC: %.3f' % mean(scores))
+    return model
 
-def 
+def weighted_logistic(X,y):
+    model = LogisticRegression(max_iter=1000,C=0.01)
+    counter = Counter(y)
+    reverse_counter={0:counter[1],1:counter[0]}
+    balance = [{0:1,1:10}, {0:1,1:100},reverse_counter]
+    param_grid = dict(class_weight=balance,
+                    #   solver=['newton-cg', 'lbfgs', 'liblinear','sag', 'saga'],
+                    #   C=[0.01,0.1,1,10,100]
+                    #   penalty=['l1', 'l2', 'elasticnet', 'none']
+                      )
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    grid = GridSearchCV(estimator=model,param_grid=param_grid,cv=cv,scoring='roc_auc')
+    grid_result = grid.fit(X, y)
+    print('Best Weighted logistic regression: %f using %s' % (grid_result.best_score_, grid_result.best_params_))
+    return model
+
+def test_model():
+    df=pd.read_csv('../input/pooled_imputed_scaled_no_na.csv')
+    y=np.array(df['dem1066'])
+    X=df[df.columns[:-1]]
+    # baseline_logistic(X,y)
+    # weighted_logistic(X,y)
+
+if __name__=='__main__':
+    test_model()
