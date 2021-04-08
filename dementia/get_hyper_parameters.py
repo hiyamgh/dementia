2992d7179b00c2de8c68ded8b5e7f91644fc7a44
Hiyam G <hkg02@mail.aub.edu>
2021-04-08 22:39:08 +0300

    updated saving/reading hyper parameters

--- dementia/get_hyper_parameters.py
+++ dementia/get_hyper_parameters.py
@@ -1,50 +1,21 @@
-import pandas as pd
 import pickle
-import os
 
-# metatrain_iterations=(1000)
-# meta_batch_sizes=(16)
-# meta_lrs=(0.001 0.1)
-# update_lrs=(0.001 0.1)
-# dim_hidden=("128, 64, 64" "128, 64" "128")
-# IFS=""
-# activation_fns=("relu")
-# num_updates=(4)
-# include_fp=("1")
-# fp_supps=(0.8 0.9 0.7)
-# weights=("1, 1" "1, 10" "1, 100" "10, 1" "100, 1")
-# IFS=""
-# sampling_strategy=("minority" "not minority" "all" "0.5" "1" "0.75")
-# IFS=""
-# USCOUNTER=1
-#
-# for miter in ${metatrain_iterations[@]}; do
-#   for mbs in ${meta_batch_sizes[@]}; do
-#     for mlr in ${meta_lrs[@]}; do
-#       for ulr in ${update_lrs[@]}; do
-#         for dh in ${dim_hidden[*]}; do
-#           for afn in ${activation_fns[@]}; do
-#             for nu in ${num_updates[@]}; do
-#               for ifp in ${include_fp[@]}; do
-#                 for fp_supp in ${fp_supps[@]}; do
-#                   for wt in ${weights[@]}; do
-#                     for ss in ${sampling_strategy[@]}; do
-#                      if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
 
 def get_hyper_parameters(outfile_name, fp=True):
     hyperparameters = {}
 
     metatrain_iterations = [1000]
     meta_batch_sizes = [16]
-    meta_lrs = [0.1, 0.001]
-    update_lrs = [0.1, 0.001]
-    dim_hidden = ["128, 64, 64", "128, 64", "128"]
+    meta_lrs = [0.1]
+    update_lrs = [0.1]
+    dim_hidden = ["128, 64", "128", "128, 64, 64"]
     activation_fns = ["relu"]
     num_updates = [4]
     include_fp = ["1"]
     fp_supps = [0.8, 0.9, 0.7]
     weights= ["1, 1", "1, 10", "1, 100", "10, 1", "100, 1"]
     sampling_strategy = ["minority", "not minority", "all", "0.5", "1", "0.75"]
+    encoding = ["catboost", "glmm", "target", "mestimator", "james", "woe"]
 
     if fp:
         count = 1
@@ -59,20 +30,22 @@ def get_hyper_parameters(outfile_name, fp=True):
                                         for fp_supp in fp_supps:
                                             for wt in weights:
                                                 for ss in sampling_strategy:
-                                                    hyperparameters["model_{}".format(count)] = {
-                                                        'miter': miter,
-                                                        'mbs': mbs,
-                                                        'mlr': mlr,
-                                                        'ulr': ulr,
-                                                        'dh': dh,
-                                                        'afn': afn,
-                                                        'nu': nu,
-                                                        'ifp': ifp,
-                                                        'fp_supp': fp_supp,
-                                                        'weights': wt,
-                                                        'sampling_strategy': ss
-                                                    }
-                                                    count += 1
+                                                    for e in encoding:
+                                                        hyperparameters["model_{}".format(count)] = {
+                                                            'miter': miter,
+                                                            'mbs': mbs,
+                                                            'mlr': mlr,
+                                                            'ulr': ulr,
+                                                            'dh': dh,
+                                                            'afn': afn,
+                                                            'nu': nu,
+                                                            'ifp': ifp,
+                                                            'fp_supp': fp_supp,
+                                                            'weights': wt,
+                                                            'sampling_strategy': ss,
+                                                            'encoding': e
+                                                        }
+                                                        count += 1
     else:
         include_fp = ["0"]
         count = 1
@@ -86,19 +59,21 @@ def get_hyper_parameters(outfile_name, fp=True):
                                     for ifp in include_fp:
                                         for wt in weights:
                                             for ss in sampling_strategy:
-                                                hyperparameters["model_{}".format(count)] = {
-                                                    'miter': miter,
-                                                    'mbs': mbs,
-                                                    'mlr': mlr,
-                                                    'ulr': ulr,
-                                                    'dh': dh,
-                                                    'afn': afn,
-                                                    'nu': nu,
-                                                    'ifp': ifp,
-                                                    'weights': wt,
-                                                    'sampling_strategy': ss
-                                                }
-                                                count += 1
+                                                for e in encoding:
+                                                    hyperparameters["model_{}".format(count)] = {
+                                                        'miter': miter,
+                                                        'mbs': mbs,
+                                                        'mlr': mlr,
+                                                        'ulr': ulr,
+                                                        'dh': dh,
+                                                        'afn': afn,
+                                                        'nu': nu,
+                                                        'ifp': ifp,
+                                                        'weights': wt,
+                                                        'sampling_strategy': ss,
+                                                        'encoding': e
+                                                    }
+                                                    count += 1
     print('number of models: {}'.format(count))
     with open('{}.p'.format(outfile_name), 'wb') as f:
         pickle.dump(hyperparameters, f, pickle.HIGHEST_PROTOCOL)
