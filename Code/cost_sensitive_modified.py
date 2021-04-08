1625f5e4560d06fbcf11b4f6312205f96e872a19
Hiyam Ghannam <hkg02@mail.aub.edu>
2021-04-08 12:49:29 +0300

    fixed bug in one class predictions

--- Code/cost_sensitive_modified.py
+++ Code/cost_sensitive_modified.py
@@ -24,10 +24,14 @@ import pickle, os
 import category_encoders as ce
 from sklearn.compose import ColumnTransformer
 import warnings
+
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
+from pandas.core.common import SettingWithCopyWarning
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 f2_score = make_scorer(fbeta_score, beta=2)
+
 
 def encode_categorical_data_supervised(X_train, y_train, X_test, cat_cols, enc_method):
     if enc_method == 'catboost':
@@ -45,7 +49,7 @@ def encode_categorical_data_supervised(X_train, y_train, X_test, cat_cols, enc_m
     elif enc_method == 'james':
         print('Encoding: {}'.format(enc_method))
         encoder = ce.JamesSteinEncoder(cols=cat_cols)
-    else: # woe
+    else:  # woe
         print('Encoding: {}'.format(enc_method))
         encoder = ce.WOEEncoder(cols=cat_cols)
 
@@ -170,7 +174,6 @@ def create_model(y, model, model_name):
 
 def print_results(model_name, best_params, y, y_predicted, df_results, encoding_strategy,
                   proba=False, one_class=False, threshold=None, topn=10):
-
     if proba:
         y_predicted = to_labels(y_predicted, threshold)
 
@@ -236,6 +239,41 @@ def print_results(model_name, best_params, y, y_predicted, df_results, encoding_
     return df_results
 
 
+def test_model_one_class(train_df, test_df, feature_importance, model_class, model_name,
+                         df_results, proba=False, one_class=False,
+                         topn=10):
+    enc_methods = ['catboost', 'glmm', 'target', 'mestimator', 'james', 'woe']
+
+    for enc in enc_methods:
+        X = train_df[feature_importance[:topn]]
+        y = train_df['dem1066']
+        # test_X=test_df[df.columns[:-1]]
+        test_X = test_df[feature_importance[:topn]]
+        test_y = test_df['dem1066']
+
+        with open('../input/columns/categorical.p', 'rb') as f:
+            categorical_cols = pickle.load(f)
+        all_cols = list(X.columns)
+        cat_cols = [c for c in categorical_cols if c in all_cols]
+
+        X, test_X, _ = encode_categorical_data_supervised(X, y, test_X, cat_cols, enc)
+        X['dem1066'] = train_df['dem1066']
+        X = X[X['dem1066'] == 0]
+        X = X.drop(['dem1066'], axis=1)
+        test_y[test_y == 1] = -1
+        test_y[test_y == 0] = 1
+
+        estimator = model
+        best_params = {}
+        estimator.fit(X)
+
+        y_predicted = estimator.predict(test_X)
+        df_results = print_results(model_name, best_params, test_y, y_predicted, df_results, enc,
+                                   proba=proba, one_class=one_class, topn=topn)
+
+    return df_results
+
+
 def test_model(train_df, test_df, feature_importance, model_class, model_name,
                df_results, proba=False, one_class=False,
                topn=10):
@@ -259,21 +297,24 @@ def test_model(train_df, test_df, feature_importance, model_class, model_name,
     enc_methods = ['catboost', 'glmm', 'target', 'mestimator', 'james', 'woe']
     model, param_grid = create_model(y, model_class, model_name)
     for enc in enc_methods:
-        pipeline = create_pipeline(model, X.columns, X, enc_method=enc, cat_cols=cat_cols)
-        cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 
         if not one_class:
+            pipeline = create_pipeline(model, X.columns, X, enc_method=enc, cat_cols=cat_cols)
+            cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
             grid = GridSearchCV(estimator=pipeline, param_grid=param_grid, cv=cv, scoring=f2_score)
             grid_result = grid.fit(np.array(X), np.array(y))
             print('Best model: %f using %s' % (grid_result.best_score_, grid_result.best_params_))
             estimator = grid_result.best_estimator_
             best_params = grid_result.best_params_
+            estimator.fit(X, y)
         else:
             estimator = model
             best_params = {}
-            X, test_X, _ = encode_categorical_data_supervised(X, y, test_X,cat_cols, enc)
+            X, test_X, _ = encode_categorical_data_supervised(X, y, test_X, cat_cols, enc)
+            estimator.fit(X)
 
-        estimator.fit(X, y)
+        # estimator.fit(X, y)
         if proba:
             yhat = estimator.predict_proba(test_X)
             probs = yhat[:, 1]
@@ -286,13 +327,13 @@ def test_model(train_df, test_df, feature_importance, model_class, model_name,
             print('Threshold=%.3f, F-measure=%.5f' % (thresholds[ix], scores[ix]))
             best_params['f2_score'] = scores[ix]
             df_results = print_results(model_name, best_params, test_y, probs, df_results, enc,
-                          proba=proba, one_class=one_class,
-                          threshold=thresholds[ix], topn=topn)
+                                       proba=proba, one_class=one_class,
+                                       threshold=thresholds[ix], topn=topn)
         else:
             y_predicted = estimator.predict(test_X)
             # y_predicted=lof_predict(estimator, X, test_X)
             df_results = print_results(model_name, best_params, test_y, y_predicted, df_results, enc,
-                          proba=proba, one_class=one_class, topn=topn)
+                                       proba=proba, one_class=one_class, topn=topn)
 
     return df_results
 
@@ -302,17 +343,17 @@ if __name__ == '__main__':
     df = pd.read_csv('../input/train_imputed_scaled.csv')
     test_df = pd.read_csv('../input/test_imputed_scaled.csv')
     feature_importance = np.array(pd.read_csv('../input/feature_importance_modified.csv')['Feature'])
-    models_dict = {}
+    # models_dict = {}
 
     for topn in [10, 20]:
 
-        out_proba = '../output_probabilistic/top{}/'.format(topn)
+        out_proba = '../output/output_probabilistic/top{}/'.format(topn)
         mkdir(out_proba)
 
-        out_regular = '../output_regular/top_{}/'.format(topn)
+        out_regular = '../output/output_regular/top_{}/'.format(topn)
         mkdir(out_regular)
 
-        out_one_class = '../output_one_class/top_{}/'.format(topn)
+        out_one_class = '../output/output_one_class/top_{}/'.format(topn)
         mkdir(out_one_class)
 
         df_proba = pd.DataFrame(columns=['model_name', 'f2', 'gmean', 'bss', 'pr_auc',
@@ -357,12 +398,12 @@ if __name__ == '__main__':
             (OneClassSVM(gamma='scale', nu=0.1), 'One Class SVM', False, True),
             (EllipticEnvelope(contamination=0.1), 'EllipticEnvelope', False, True),
             (IsolationForest(contamination=0.1), 'IsolationForest', False, True),
-            (LocalOutlierFactor(contamination=0.1), 'LocalOutlierFactor', False, True)]:
+            (LocalOutlierFactor(contamination=0.1, novelty=True), 'LocalOutlierFactor', False, True)]:
+            print('======================= Model: {} ======================='.format(model_name))
+            df_one_class = test_model_one_class(df, test_df, feature_importance, model, model_name,
+                                                df_one_class, proba=proba, one_class=one_class, topn=topn)
 
-            df_one_class = test_model(df, test_df, feature_importance, model, model_name,
-                                      df_one_class, proba=proba, one_class=one_class, topn=topn)
-
-            df_one_class = df_one_class.sort_values(by='f2')
+            df_one_class = df_one_class.sort_values(by='f2', ascending=False)
             df_one_class.to_csv(os.path.join(out_one_class, 'one_class_results.csv'), index=False)
 
 
