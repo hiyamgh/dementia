0475344339782ac048e5fe9debbf6137948e9b95
Hiyam G <hkg02@mail.aub.edu>
2021-05-15 19:37:46 +0300

    resolved job array error in shell scripts

--- arml/shell/run_dmtop10_batch3.sh
+++ arml/shell/run_dmtop10_batch3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=dmarml
+#SBATCH --job-name=dmarmlb3
 #SBATCH --account=hkg02
 #SBATCH --partition=normal
 #SBATCH --nodes=1
@@ -8,13 +8,13 @@
 #SBATCH --mem=16000
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hkg02@mail.aub.edu
-#SBATCH --array=1801-2700%5
+#SBATCH --array=1-900%5
 
 module load python/3
 module load python/tensorflow-1.14.0
 
 metatrain_iterations=(1000)
-meta_batch_sizes=(32)
+meta_batch_sizes=(16)
 meta_lrs=(0.1)
 update_lrs=(0.1)
 dim_hidden=("128, 64" "128" "128, 64, 64")
@@ -29,6 +29,8 @@ IFS=""
 encoding=("catboost" "glmm" "target" "mestimator" "james" "woe")
 IFS=""
 num_vertex=(4 6 8)
+num_filters=(16 32)
+emb_loss_weight=(0.01 0.1)
 USCOUNTER=1
 
 for miter in ${metatrain_iterations[@]}; do
@@ -42,12 +44,17 @@ for miter in ${metatrain_iterations[@]}; do
                 for wt in ${weights[@]}; do
                   for ss in ${sampling_strategy[@]}; do
                     for e in ${encoding[@]}; do
-                        if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
-                          echo "USCOUNTER: " $USCOUNTER
-                          echo "metatrain_iterations: ${miter} meta_batch_size: ${mbs} meta_lr: ${mlr} update_batch_size: ${mbs} update_lr: ${ulr} num_updates: ${nu} dim_hidden: ${dh} model_num: $SLURM_ARRAY_TASK_ID activation_fn: ${afn} num_vertex ${nv} weights_vector ${wt} --sampling_strategy ${ss} --top_features 10 --logdir dm_top10/ --categorical_encoding ${e}"
-                          python main_dm.py --metatrain_iterations ${miter} --meta_batch_size ${mbs} --meta_lr ${mlr} --update_batch_size ${mbs} --update_lr ${ulr} --num_updates ${nu} --dim_hidden ${dh} --model_num $SLURM_ARRAY_TASK_ID --activation_fn ${afn} --categorical_encoding ${e} --num_vertex ${nv} --weights_vector ${wt} --sampling_strategy ${ss} --top_features 10 --logdir "dm_top10/"
-                        fi
-                        USCOUNTER=$(expr $USCOUNTER + 1)
+                      for nf in ${num_filters[@]}; do
+                        for elw in ${emb_loss_weight[@]}; do
+                          if ((USCOUNTER=SLURM_ARRAY_TASK_ID+1800)); then
+                              echo "USCOUNTER: " $USCOUNTER
+                              echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+                              echo "main_dm.py --metatrain_iterations ${miter} --meta_batch_size ${mbs} --meta_lr ${mlr} --update_batch_size ${mbs} --update_lr ${ulr} --num_updates ${nu} --dim_hidden ${dh} --model_num $USCOUNTER --activation_fn ${afn} --categorical_encoding ${e} --num_vertex ${nv} --weights_vector ${wt} --sampling_strategy ${ss} --top_features 10 --logdir "dm_top10/" --num_filters ${nf} --emb_loss_weight ${elw}"
+                              python main_dm.py --metatrain_iterations ${miter} --meta_batch_size ${mbs} --meta_lr ${mlr} --update_batch_size ${mbs} --update_lr ${ulr} --num_updates ${nu} --dim_hidden ${dh} --model_num $USCOUNTER --activation_fn ${afn} --categorical_encoding ${e} --num_vertex ${nv} --weights_vector ${wt} --sampling_strategy ${ss} --top_features 10 --logdir "dm_top10/" --num_filters ${nf} --emb_loss_weight ${elw}
+                             fi
+                             USCOUNTER=$(expr $USCOUNTER + 1)
+                        done
+                      done
                     done
                   done
                 done
--- arml/shell/run_fn.sh
+++ arml/shell/run_fn.sh
@@ -8,20 +8,22 @@
 #SBATCH --mem=16000
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hkg02@mail.aub.edu
-#SBATCH --array=1-360%5
+#SBATCH --array=1-720%5
 
 module load python/3
 module load python/tensorflow-1.14.0
 
 metatrain_iterations=(1000)
-meta_batch_sizes=(32 16)
-meta_lrs=(0.001 0.1)
-update_lrs=(0.001 0.1)
+meta_batch_sizes=(32)
+meta_lrs=(0.1)
+update_lrs=(0.1)
 dim_hidden=("128, 64" "128" "128, 64, 64")
 IFS=""
 activation_fns=("relu" "sigmoid" "tanh" "softmax" "swish")
-num_updates=(4)
-num_vertex=(4 6 8)
+num_updates=(2 4 10)
+num_vertex=(2 4 8 10)
+num_filters=(16 32)
+emb_loss_weight=(0.01 0.1)
 USCOUNTER=1
 
 for miter in ${metatrain_iterations[@]}; do
@@ -32,12 +34,17 @@ for miter in ${metatrain_iterations[@]}; do
           for afn in ${activation_fns[@]}; do
             for nu in ${num_updates[@]}; do
                for nv in ${num_vertex[@]}; do
-                   if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
-                   echo "USCOUNTER: " $USCOUNTER
-                   echo "pretrain_iterations: 0 metatrain_iterations: ${miter} meta_batch_size: ${mbs} meta_lr: ${mlr} update_batch_size: ${mbs} update_lr: ${ulr} num_updates: ${nu} dim_hidden: ${dh} model_num: $SLURM_ARRAY_TASK_ID activation_fn: ${afn} logdir: fn/ num_vertex ${nv}"
-                   python main_fn.py --metatrain_iterations ${miter} --meta_batch_size ${mbs} --meta_lr ${mlr} --update_batch_size ${mbs} --update_lr ${ulr} --num_updates ${nu} --dim_hidden ${dh} --model_num $SLURM_ARRAY_TASK_ID --activation_fn ${afn} --logdir "fn/" --num_vertex ${nv}
-                   fi
-                  USCOUNTER=$(expr $USCOUNTER + 1)
+                 for nf in ${num_filters[@]}; do
+                   for elw in ${emb_loss_weight[@]}; do
+                      if((USCOUNTER=SLURM_ARRAY_TASK_ID)); then
+                           echo "USCOUNTER: " $USCOUNTER
+                           echo "$SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+                           echo "main_fn.py --metatrain_iterations ${miter} --meta_batch_size ${mbs} --meta_lr ${mlr} --update_batch_size ${mbs} --update_lr ${ulr} --num_updates ${nu} --dim_hidden ${dh} --model_num $USCOUNTER --activation_fn ${afn} --logdir "fn_batch2/" --num_vertex ${nv} --num_filters ${nf} --emb_loss_weight ${elw}"
+                           python main_fn.py --metatrain_iterations ${miter} --meta_batch_size ${mbs} --meta_lr ${mlr} --update_batch_size ${mbs} --update_lr ${ulr} --num_updates ${nu} --dim_hidden ${dh} --model_num $USCOUNTER --activation_fn ${afn} --logdir "fn_batch2/" --num_vertex ${nv} --num_filters ${nf} --emb_loss_weight ${elw}
+                      fi
+                      USCOUNTER=$(expr $USCOUNTER + 1)
+                   done
+                 done
                done
              done
           done
