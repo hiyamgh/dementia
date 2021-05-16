12114aa2be8a8e039ab6a539e67c5c6130333048
Hiyam G <hkg02@mail.aub.edu>
2021-05-16 12:28:42 +0300

    added shell scripts for reptile

---
+++ reptile/shell/run_dmtop20_b1.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=dm20r1
+#SBATCH --account=hkg02
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hkg02@mail.aub.edu
+#SBATCH --array=1-900%5
+
+module load python/3
+module load python/tensorflow-1.14.0
+
+dim_hidden=("128, 64" "128" "128, 64, 64")
+IFS=""
+activation_fns=("relu")
+IFS=""
+shots=(32)
+train_shots=(32)
+inner_batch=(5)
+learning_rate=(0.1)
+meta_batch=(1 5 10)
+weights=("1, 1" "1, 10" "1, 100" "10, 1" "100, 1")
+IFS=""
+sampling_strategy=("minority" "not minority" "all" "0.5" "1" "0.75")
+IFS=""
+encoding=("catboost" "glmm" "target" "mestimator" "james" "woe")
+IFS=""
+USCOUNTER=1
+
+for dh in ${dim_hidden[*]}; do
+    for af in ${activation_fns[*]}; do
+        for s in ${shots[@]}; do
+            for ts in ${train_shots[@]}; do
+                for ib in ${inner_batch[@]}; do
+                    for lr in ${learning_rate[@]}; do
+                        for mb in ${meta_batch[@]}; do
+                            for w in ${weights[@]}; do
+                                for ss in ${sampling_strategy[@]}; do
+                                    for e in ${encoding[@]}; do
+                                        if((USCOUNTER=SLURM_ARRAY_TASK_ID)); then
+                                            echo "USCOUNTER: " $USCOUNTER
+                                            echo "$SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+                                            echo "main_dm.py --dim_hidden ${dh} --activation_fn ${af} --shots ${s} --train-shots ${ts} --inner-batch ${ib} --learning-rate ${lr} --save_dir "dm_top20/" --meta-batch ${mb} --weights_vector ${w} --sampling_strategy ${ss} --categorical_encoding ${e} --top_features 20"
+                                            python main_dm.py --dim_hidden ${dh} --activation_fn ${af} --shots ${s} --train-shots ${ts} --inner-batch ${ib} --learning-rate ${lr} --save_dir "dm_top20/" --meta-batch ${mb} --weights_vector ${w} --sampling_strategy ${ss} --categorical_encoding ${e} --top_features 20
+                                        fi
+                                        USCOUNTER=$(expr $USCOUNTER + 1)
+                                    done
+                                done
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
---
+++ reptile/shell/run_fn.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=fnrept
+#SBATCH --account=hkg02
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hkg02@mail.aub.edu
+#SBATCH --array=1-810%5
+
+module load python/3
+module load python/tensorflow-1.14.0
+
+dim_hidden=("128, 64" "128" "128, 64, 64")
+IFS=""
+activation_fns=("relu" "sigmoid" "tanh" "softmax" "swish")
+IFS=""
+shots=(32)
+train_shots=(0 16 32)
+inner_batch=(5 10 15)
+learning_rate=(0.1 0.001)
+meta_batch=(1 5 10)
+
+USCOUNTER=1
+for dh in ${dim_hidden[*]}; do
+    for af in ${activation_fns[*]}; do
+        for s in ${shots[@]}; do
+            for ts in ${train_shots[@]}; do
+                for ib in ${inner_batch[@]}; do
+                    for lr in ${learning_rate[@]}; do
+                        for mb in ${meta_batch[@]}; do
+                            if((USCOUNTER=SLURM_ARRAY_TASK_ID)); then
+                                echo "USCOUNTER: " $USCOUNTER
+                                echo "$SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+                                echo "main_fn.py --dim_hidden ${dh} --activation_fn ${af} --shots ${s} --train-shots ${ts} --inner-batch ${ib} --learning-rate ${lr} --save_dir "fn/" --meta-batch ${mb}"
+                                python main_fn.py --dim_hidden ${dh} --activation_fn ${af} --shots ${s} --train-shots ${ts} --inner-batch ${ib} --learning-rate ${lr} --save_dir "fn/" --meta-batch ${mb}
+                            fi
+                            USCOUNTER=$(expr $USCOUNTER + 1)
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
