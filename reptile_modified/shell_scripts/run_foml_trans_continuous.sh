da0aba5fd1d76197275b582741eb6e7d9e94e75c
Hiyam G <hkg02@mail.aub.edu>
2021-09-09 11:54:29 +0300

    added latest script FOMAML

--- reptile_modified/shell_scripts/run_foml_trans_continuous.sh
+++ reptile_modified/shell_scripts/run_foml_trans_continuous.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=fotrcont
 #SBATCH --account=hkg02
-#SBATCH --partition=normal
+#SBATCH --partition=large
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
