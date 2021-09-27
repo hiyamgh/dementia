eb80d42285409858dca3ac8afc2a194a951f460a
Hiyam Ghannam <hkg02@mail.aub.edu>
2021-09-27 15:28:47 +0300

    added code to get dictionary mapping indices to hyperparameters

---
+++ Reptile/get_hyperparameters.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=gethyps
+#SBATCH --account=hkg02
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hkg02@mail.aub.edu
+
+module load python/3
+
+python get_hyperparameters.py
