6b516390f3d4168b62d174e0162382839ec01794
Hiyam G <hkg02@mail.aub.edu>
2022-01-03 10:03:20 +0200

    refactored top 10 and top 20 columns

--- input/save_top10_top20_notebook.py
+++ input/save_top10_top20_notebook.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('feature_importance_modified.csv')
-top10 = df.loc[:10, ['Feature', 'description']]
-top20 = df.loc[:20, ['Feature', 'description']]
+top10 = df.loc[0:9, ['Feature', 'description']] # in loc, start and stop are both included
+top20 = df.loc[0:19, ['Feature', 'description']]# in loc, start and stop are both included
 
 top10.to_csv('codebooks/top10.csv', index=False)
 top20.to_csv('codebooks/top20.csv', index=False)
