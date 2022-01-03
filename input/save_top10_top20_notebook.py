bcdd836d9fcac679cd8ad497c5636a411cfbd806
Hiyam G <hkg02@mail.aub.edu>
2022-01-03 10:11:24 +0200

    final cleanup to slicing

--- input/save_top10_top20_notebook.py
+++ input/save_top10_top20_notebook.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 df = pd.read_csv('feature_importance_modified.csv')
-top10 = df.loc[0:9, ['Feature', 'description']] # in loc, start and stop are both included
-top20 = df.loc[0:19, ['Feature', 'description']]# in loc, start and stop are both included
+features = list(df['Feature'])
+f10 = features[:10]
+f20 = features[:20]
 
+top10 = df[:10].drop(['Importance'], axis=1)
+top20 = df[:20].drop(['Importance'], axis=1)
+
+# top10 = df.loc[0:10, ['Feature', 'description']] # in loc, start and stop are both included
+# top20 = df.loc[0:20, ['Feature', 'description']]# in loc, start and stop are both included
+#
 top10.to_csv('codebooks/top10.csv', index=False)
 top20.to_csv('codebooks/top20.csv', index=False)
